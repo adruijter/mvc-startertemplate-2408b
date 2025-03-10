7e0b7851807f68582278533211168fa5c7f9355d
adruijter <adruijter@gmail.com>
2025-03-10 20:43:41 +0100

    Alles werkt

--- app/models/SmartphonesModel.php
+++ app/models/SmartphonesModel.php
@@ -21,7 +21,7 @@ class SmartphonesModel
                        ,SMPH.Geheugen
                        ,SMPH.Besturingssysteem
                        ,FORMAT(SMPH.Schermgrootte, 2, "nl_NL") as Schermgrootte
-                       ,DATE_FORMAT(SMPH.Releasedatum, "%d/%m/%Y") as Releasedatum
+                       ,DATE_FORMAT(SMPH.Releasedatum, "%d-%m-%Y") as Releasedatum
                        ,SMPH.Gewicht
                        ,IF (Simlockvrij = 1, "Ja", "Nee") as Simlockvrij
                 FROM smartphones as SMPH
