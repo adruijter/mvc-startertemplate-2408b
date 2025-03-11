fc7cf3ea64fc51579340d4ff41c7d312eb835900
adruijter <adruijter@gmail.com>
2025-03-11 14:26:23 +0100

    sneakers zijn af

--- app/views/homepages/index.php
+++ app/views/homepages/index.php
@@ -10,7 +10,8 @@
 
             <h3><?php echo $data['title']; ?></h3>
 
-            <a href="<?= URLROOT; ?>/smartphones/index ">Toon smartphones</a>
+            <a href="<?= URLROOT; ?>/smartphones/index ">Toon smartphones</a> |
+            <a href="<?= URLROOT; ?>/sneakers/index">Mooiste Sneakers</a>
 
         </div>
         
